642566aa1a23bad1f2afe89d21fdcc1a924a2e16
muchamadagushh <muchamadagush@gmail.com>
2023-10-01 05:52:00 +0700

    feat(auth): middleware auth

---
+++ src/helpers/Token.ts
@@ -0,0 +1,83 @@
+import { Request } from 'express'
+import jwt, {
+  TokenExpiredError,
+  JsonWebTokenError,
+  NotBeforeError,
+} from 'jsonwebtoken'
+import { isEmpty } from 'lodash'
+
+require('dotenv').config()
+
+const {
+  JWT_SECRET_ACCESS_TOKEN,
+  JWT_SECRET_REFRESH_TOKEN,
+}: {
+  [key: string]: string | undefined
+} = process.env
+
+/**
+ *
+ * @param headers - Get Token from headers
+ */
+function getToken(headers: any) {
+  if (headers && headers.authorization) {
+    const parted = headers.authorization.split(' ')
+
+    // Check Bearer xxx || JWT xxx
+    if (parted[0] === 'Bearer' || parted[0] === 'JWT') {
+      if (parted.length === 2) {
+        return parted[1]
+      }
+    }
+
+    return null
+  }
+  return null
+}
+
+/**
+ *
+ * @param req - Request
+ */
+function currentToken(req: Request) {
+  const getCookie = req.getCookies()
+  const getHeaders = req.getHeaders()
+
+  let curToken = ''
+  if (!isEmpty(getCookie.token)) {
+    curToken = getCookie.token
+  } else {
+    curToken = getToken(getHeaders)
+  }
+
+  return curToken
+}
+
+/**
+ *
+ * @param token - Verify Token
+ */
+function verifyAccessToken(token: string) {
+  try {
+    if (!token) {
+      return { data: null, message: 'Unauthorized!' }
+    }
+
+    const data = jwt.verify(token, JWT_SECRET_ACCESS_TOKEN as string)
+    return { data, message: 'Token sudah diverifikasi' }
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      return { data: null, message: `Session anda telah berakhir` }
+    }
+
+    if (err instanceof JsonWebTokenError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof NotBeforeError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+  }
+}
+
+export { getToken, currentToken, verifyAccessToken }
---
+++ src/middlewares/AuthorizationSuperAdmin.ts
@@ -0,0 +1,32 @@
+import { isEmpty } from 'lodash'
+import { NextFunction, Request, Response } from 'express'
+import { currentToken, verifyAccessToken } from '../helpers/Token'
+import asyncHandler from '../helpers/asyncHandler'
+import UserService from '../controllers/User/user.service'
+import ResponseError from '../modules/Response/ResponseError'
+
+async function AuthorizationSuperAdmin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const getToken = currentToken(req)
+  const token: any = verifyAccessToken(getToken)
+
+  if (isEmpty(token?.data)) {
+    throw new ResponseError.Unauthorized(token?.message)
+  }
+
+  const userData = await UserService.verifyActiveUser(token?.data?.id)
+
+  if (userData.RoleId !== 'Super Admin') {
+    throw new ResponseError.Forbidden(
+      'this endpoint can only be accessed by users who have the superadmin role'
+    )
+  }
+
+  req.setState({ userData })
+  next()
+}
+
+export default asyncHandler(AuthorizationSuperAdmin)
