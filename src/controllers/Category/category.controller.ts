797d4bf2a8960e0f7a0c0a68fd5b9963fa91793d
muchamadagushh <muchamadagush@gmail.com>
2023-10-01 06:54:13 +0700

    feat(category): endpoint create

--- src/controllers/Category/category.controller.ts
+++ src/controllers/Category/category.controller.ts
@@ -3,17 +3,36 @@ import routes from '../../routes/public'
 import asyncHandler from '../../helpers/asyncHandler'
 import BuildResponse from '../../modules/Response/BuildResponse'
 import CategoryService from '../../controllers/Category/category.service'
-import AuthorizationSuperAdmin from '../../middlewares/AuthorizationSuperAdmin'
+import Authorization from '../../middlewares/Authorization'
+import useValidation from '../../helpers/useValidation'
+import categorySchema from './category.schema'
 
 const categoryService = new CategoryService()
 
 routes.get(
   '/category',
-  AuthorizationSuperAdmin,
+  Authorization,
   asyncHandler(async function getAll(req: Request, res: Response) {
     const data = await categoryService.getAll(req)
     const buildResponse = BuildResponse.get(data)
 
     res.status(200).json(buildResponse)
+  })
+)
+
+routes.post(
+  '/category',
+  Authorization,
+  asyncHandler(async function create(req: Request, res: Response): Promise<any> {
+    const formData = req.getBody()
+    const txn = await req.getTransaction()
+    const validatedData = useValidation(categorySchema.create, formData)
+
+    const data = await categoryService._model.create(validatedData, { transaction: txn })
+
+    await txn.commit()
+
+    const buildResponse = BuildResponse.created({ data })
+    return res.status(201).json(buildResponse)
   })
 )
