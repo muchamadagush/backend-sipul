922f95d4c8097024d06a677546e4a5d25b3fdc20
muchamadagushh <muchamadagush@gmail.com>
2023-10-03 20:35:45 +0700

    feat(auth): endpoint get profile

--- src/controllers/Auth/auth.controller.ts
+++ src/controllers/Auth/auth.controller.ts
@@ -3,6 +3,8 @@ import asyncHandler from '../../helpers/asyncHandler'
 import routes from '../../routes/public'
 import AuthService from './auth.service'
 import BuildResponse from '../../modules/Response/BuildResponse'
+import Authorization from '../../middlewares/Authorization'
+import UserService from '../User/user.service'
 
 routes.post(
   '/auth/login',
@@ -21,5 +23,18 @@ routes.post(
         secure: process.env.NODE_ENV === 'production',
       })
       .json(buildResponse)
+  })
+)
+
+routes.get(
+  '/profile',
+  Authorization,
+  asyncHandler(async function login(req: Request, res: Response): Promise<any> {
+    const userData = req.getState('userData')
+
+    const data = await UserService.getUserById(userData.id)
+
+    const buildResponse = BuildResponse.get({ data })
+    return res.status(200).json(buildResponse)
   })
 )
